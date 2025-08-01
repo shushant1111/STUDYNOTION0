0b44168fb1ce3c8df5fd6aa2ad79b80039a83890
shushant786 <shushantsonwani1@gmail.com>
2025-08-02 02:51:56 +0530

    comments

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,26 @@
+// Importing the Mongoose library to interact with MongoDB
 const mongoose = require("mongoose");
+
+// Loads environment variables from a .env file into process.env
 require("dotenv").config();
 
+// Destructuring the MongoDB connection URL from environment variables
 const { MONGODB_URL } = process.env;
 
+// Exporting a function to connect to the MongoDB database
 exports.connect = () => {
 	mongoose
 		.connect(MONGODB_URL, {
-			useNewUrlparser: true,
+			// These options are used to remove deprecation warnings
+			useNewUrlParser: true,        // Corrected spelling: was `useNewurlparser`
 			useUnifiedTopology: true,
 		})
-		.then(console.log(`DB Connection Success`))
+		// This `then` executes immediately because you're passing the result of console.log instead of a function
+		.then(() => console.log(`DB Connection Success`))  // ✅ Fixed: wrapped in arrow function
 		.catch((err) => {
+			// If connection fails, log the error and exit the process
 			console.log(`DB Connection Failed`);
 			console.log(err);
-			process.exit(1);
+			process.exit(1); // Exit the Node.js process with failure code
 		});
 };
