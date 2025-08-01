32e20a90f592a975bf229fe99e69efd3d409adbb
shushant786 <shushantsonwani1@gmail.com>
2025-08-02 02:24:33 +0530

    comments

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,6 +1,11 @@
+// Importing the Razorpay library
 const Razorpay = require("razorpay");
 
+// Creating and exporting a Razorpay instance with API credentials
 exports.instance = new Razorpay({
+    // Public key used to identify the Razorpay account
 	key_id: process.env.RAZORPAY_KEY,
+
+    // Secret key used for authentication (keep it secure!)
 	key_secret: process.env.RAZORPAY_SECRET,
 });
